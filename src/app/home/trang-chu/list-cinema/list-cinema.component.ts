b4db47b4162c95b80d996ab2ac73942840d4ef64
= <=>
2020-05-13 17:32:36 +0700

    new file

--- src/app/home/trang-chu/list-cinema/list-cinema.component.ts
+++ src/app/home/trang-chu/list-cinema/list-cinema.component.ts
@@ -9,6 +9,10 @@ import { CinemasService } from "src/app/core/services/cinemas.service";
 export class ListCinemaComponent implements OnInit {
   rapDangChon: any;
   lichChieu: any;
+  danhSachRap: any;
+  danhSachPhim: any;
+  xuatChieuPhim: any;
+
   constructor(
     public cinemaService: CinemasService,
   ) { }
@@ -20,7 +24,6 @@ export class ListCinemaComponent implements OnInit {
     const heThongRap = this.cinemaService.heThongRap();
     if (heThongRap.length === 0) {
       this.cinemaService.layThongTinHeThongRap().subscribe(res => {
-        console.log(res[0].maHeThongRap);
         // Gọi api lấy cụm rạp theo hệ thống
         this.cinemaService.layCumRapTheoHeThong(res[0].maHeThongRap).subscribe();
         // Gọi api lấy lịch chiếu theo hệ thống rạp
@@ -44,5 +47,15 @@ export class ListCinemaComponent implements OnInit {
       item.maCumRap === maCumRap
     );
     console.log(this.rapDangChon, this.lichChieu);
+
+    //tách data dsPhim và dsRap
+    const { danhSachRap, ...chiTietRap } = this.rapDangChon;
+    this.danhSachRap = danhSachRap;
+    const { danhSachPhim, ...ctRap } = this.lichChieu;
+    this.danhSachPhim = danhSachPhim;
+
+    // console.log(this.danhSachRap, this.danhSachPhim);
+
+
   }
 }
