3df9d4f7dafe74b11cae5ce41f4ae191d74bedb0
= <=>
2020-05-22 20:40:40 +0700

    new file

--- src/app/admin/dashboard-movie/dashboard-movie.component.ts
+++ src/app/admin/dashboard-movie/dashboard-movie.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from "@angular/core";
 import { UsersService } from "src/app/core/services/users.service";
 import { MoviesService } from "src/app/core/services/movies.service";
@@ -10,7 +9,7 @@ import { UserRegister } from "src/app/core/models/user-register";
   styleUrls: ["./dashboard-movie.component.scss"]
 })
 export class DashboardMovieComponent implements OnInit {
-  constructor(private listMovie: MoviesService) {}
+  constructor(private listMovie: MoviesService) { }
   danhSachPhim: any[];
   searchDSPhim: string;
   ngOnInit(): void {
@@ -28,20 +27,4 @@ export class DashboardMovieComponent implements OnInit {
     this.startPageNumberDS = pageNum * 5;
     this.endPageNumberDS = this.startPageNumberDS + 5;
   }
-=======
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard-movie',
-  templateUrl: './dashboard-movie.component.html',
-  styleUrls: ['./dashboard-movie.component.scss']
-})
-export class DashboardMovieComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
->>>>>>> 4864f36ed29054605df6d34f3ee640a3c2e3566b
 }
