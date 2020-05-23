e1011fe1c72e27477247949b2809bece162de99f
thanh dan <54978430+maithanhdan@users.noreply.github.com>
2020-05-23 16:01:12 +0700

    update

--- src/app/home/checkout/ghe/ghe.component.ts
+++ src/app/home/checkout/ghe/ghe.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { CheckoutComponent } from '../checkout.component';
 @Component({
   selector: "app-ghe",
   templateUrl: "./ghe.component.html",
